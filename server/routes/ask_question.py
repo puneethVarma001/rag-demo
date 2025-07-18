b054dacbd8ad9e452259ee4e08776313163570c7
puneethVarma001 <puneeth.varma@webloretech.com>
2025-07-18 23:26:11 +0530

    Update ask_question.py

--- server/routes/ask_question.py
+++ server/routes/ask_question.py
@@ -20,7 +20,8 @@ async def ask_question(question: str = Form(...)):
 
         # Embed model + Pinecone setup
         pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
-        index = pc.Index(os.environ["PINECONE_INDEX_NAME"])
+        PINECONE_INDEX_NAME = "medicalindex" 
+        index = pc.Index(PINECONE_INDEX_NAME)
         embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         embedded_query = embed_model.embed_query(question)
         res = index.query(vector=embedded_query, top_k=3, include_metadata=True)
